265076a320dd78103e3af60d2b41f887d3e78b11
jimbook <tianh17@lzu.edu.cn>
2020-11-20 15:19:51 +0800

    add 3D model and more function

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 from dataLayer.calculateForPoca import *
-from poca import POCA_Analysis
 from dataLayer.baseCore import h5Data
+from poca import *
 if __name__ == '__main__':
     # d = pd.read_csv("./tmpStorage/channelReplace.csv")
     data_L = h5Data("./testData/h5Data/tempData_10.28_10_20_54.h5","r")
     data_Z = h5Data("./testData/h5Data/tempData_10.27_14_12_02.h5","r")
     data_U = h5Data("./testData/h5Data/tempData_10.30_13_19_39.h5", "r")
-    data = data_L.getData(-2)
+    data = data_Z.getData(-2)
     fd = pocaAnalizy(data)
-    # pos = fd.HitPositions
-    # Ana = POCA_Analysis()
-    # for i in pos:
-    #     _df = i.reshape((12,))
-    #     Ana.run_poca(_df)
-    # Ana.poca_imaging()
+
+    pos = fd.HitPositions
+    Ana = POCA_Analysis()
+    for i in pos:
+        _df = i.reshape((12,))
+        Ana.run_poca(_df)
+    Ana.poca_imaging()
 
     # data = pd.read_csv('./testData/csvData/tempData_10.27_14_12_02poca_data.csv', header=0, index_col=0, sep='\t')
     # pos = data.values[:,:12].reshape((-1,4,3))
@@ -24,16 +25,49 @@ if __name__ == '__main__':
     # points_ratio = ratio_main(_poca,pd.read_csv("./tmpStorage/detectPlace.csv",header=0,index_col=0).values)
     # print("poca event:{},ratio running time:{}s".format(_poca.shape[0],time.time()-start))
     # print(points_ratio.shape)
-
+    r = np.random.randn(100, 3)
+    H, edges = np.histogramdd(r, bins=(5, 8, 4))
+    # print(H)
+    # print(np.sum(H))
     # 3D
     poca = fd.pocaPositions
     poca = poca[(poca[:,3] > 10) & (poca[:,3] <30)]
     poca = poca[(poca[:,2] > 600) & (poca[:,2] < 850)]
+    # hist,edges = np.histogramdd((poca[:,0],poca[:,1],poca[:,2]),
+    #                             bins=(np.arange(200,np.max(poca[:,0]),3),
+    #                                   np.arange(200,np.max(poca[:,1]),3),
+    #                                   np.arange(600,850,3)),
+    #                             weights=poca[:,3]/10)
+    # print(hist)
+    # print(np.sum(hist))
 
-    from pyqtgraph.Qt import QtCore, QtGui
+
+    from pyqtgraph.Qt import QtGui
     import pyqtgraph.opengl as gl
     import pyqtgraph as pg
     app = QtGui.QApplication([])
+    # win = QtGui.QMainWindow()
+    # win.resize(800, 800)
+    # imv = pg.ImageView()
+    # win.setCentralWidget(imv)
+    # win.show()
+    # win.setWindowTitle('pyqtgraph example: ImageView')
+    # imv.setImage(hist, xvals=edges[0])
+    #
+    # ## Set a custom color map
+    # colors = [
+    #     (0, 0, 0),
+    #     (45, 5, 61),
+    #     (84, 42, 55),
+    #     (150, 87, 60),
+    #     (208, 171, 141),
+    #     (255, 255, 255)
+    # ]
+    # cmap = pg.ColorMap(pos=np.linspace(0.0, 1, 6), color=colors)
+    # imv.setColorMap(cmap)
+
+
+
     w = gl.GLViewWidget()
     w.opts['distance'] = 20
     w.show()
@@ -44,7 +78,7 @@ if __name__ == '__main__':
                            np.array([[1,1,1,0.3],[1,1,0,0.6],[0,0,1,0.6],[0,0,0,0.6]])
                            ,pg.ColorMap.RGB).mapToFloat(poca[:,3])
     colorMap[:,3] = 0.006
-    sp2 = gl.GLScatterPlotItem(pos=poca[:,:3]/100, color=colorMap*100, size=poca[:,3]/np.max(poca[:,3]) * 10)
+    sp2 = gl.GLScatterPlotItem(pos=poca[:,:3]/100, color=colorMap*100, size=poca[:,3]/np.max(poca[:,3]) * 15)
     w.addItem(sp2)
     # colorMap = pg.ColorMap(np.array([0, 10, 20, 30]),
     #                        np.array([[1, 1, 1, 0.3], [1, 1, 0, 0.6], [0, 0, 1, 0.6], [0, 0, 0, 0.6]]),
--- threeD.py
+++ threeD.py
@@ -1,58 +1,162 @@
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph.opengl as gl
+import pyqtgraph as pg
 import numpy as np
-app = QtGui.QApplication([])
-w = gl.GLViewWidget()
-w.opts['distance'] = 20
-w.show()
-w.setWindowTitle('3D example')
+from dataLayer.baseCore import h5Data
+from PyQt5.Qt import QApplication
+from PyQt5 import Qt
+from PyQt5.QtWidgets import QWidget
+import sys
+from UI.GL3D import Ui_Form
+from dataLayer.calculateForPoca import *
+class GL3DWidget(QWidget,Ui_Form):
+    def __init__(self,*args,**kwargs):
+        super(GL3DWidget, self).__init__(*args,**kwargs)
+        self.setupUi(self)
+        self.retranslateUi(self)
+        self.setMore()
+        self.setEvent()
 
-g = gl.GLGridItem()
-w.addItem(g)
-#1.red,green and blue
-pos = np.empty((53,3))
-size = np.empty(53)
-color = np.empty((53,4))
+    def setMore(self):
+        # define parameter
+        self.zoom = 30
+        self.translate = np.array([320, 350, 600])
+        self.modelList = []
+        # set color map
+        self.widget_colorMap.setOrientation("right")
+        colorMap = pg.ColorMap(np.array([0, 0.2, 0.5, 0.8]),
+                               np.array([[0, 0, 255,255*0.6],
+                                         [0, 133, 133,255*0.6],
+                                         [255, 155, 0,255*0.6],
+                                         [255, 0, 0,255*0.6]])
+                               , pg.ColorMap.RGB)
+        self.widget_colorMap.setColorMap(colorMap)
+        # data
+        self.triggerPos = np.array([])
+        self.pocaPos = np.array([[0,0,0,0]])
+        # add 3D data item
+        self.scatterItem = gl.GLScatterPlotItem()
+        self.widget_GL.addItem(self.scatterItem)
+        # add 3D auxiliary item
+        grid = gl.GLGridItem()
+        #grid.setSize(x=7,y=7)
+        #grid.translate(dx=5,dy=5,dz=5)
+        self.widget_GL.addItem(grid)
+        vertex = np.array([[-10,10,0],[-10,10,10]])
+        for i in range(8):
+            self.widget_GL.addItem(gl.GLLinePlotItem(pos=np.array(vertex.tolist()),color=(255, 255, 255, 76.5),width=0.05,antialias=True))
+            if i == 3:
+                vertex[0,2] += 10
+                vertex[0,1] *= -1
+            elif i < 3:
+                vertex[:, i%2] *= -1
+            else:
+                vertex[i%2,:2] *= -1
 
-pos[0] = (1,0,0)
-size[0] = 0.5
-color[0] = (1.0,0.,0.,0.5)
+        # add model
+        self.addModel()
 
-pos[1] = (0,1,0)
-size[1] = 0.2
-color[1] = (0.,0.,1.,0.5)
+    def setEvent(self):
+        self.widget_colorMap.sigGradientChanged.connect(self.plotUpdate)
+        self.doubleSpinBox_minZ.valueChanged.connect(self.plotUpdate)
+        self.doubleSpinBox_maxZ.valueChanged.connect(self.plotUpdate)
+        self.doubleSpinBox_minAngle.valueChanged.connect(self.plotUpdate)
+        self.doubleSpinBox_maxAngle.valueChanged.connect(self.plotUpdate)
 
-pos[2] = (0,0,1)
-size[2] = 2./3
-color[2] = (0.,1.,0.,0.5)
+        self.checkBox_showModel.clicked.connect(self.setModelVisible)
+        self.checkBox_showPoca.clicked.connect(self.scatterItem.setVisible)
 
-z = 0.5
-d = 6.0
-for i in range(3,53):
-    pos[i] = (0,0,z)
-    size[i] = 2./d
-    color[i] = (0.,1.,0.,0.5)
-    z *= 0.5
-    d *= 2.0
 
-# sp1 = gl.GLScatterPlotItem(pos=pos, size=size, color=color, pxMode=False)
-# sp1.translate(5,5,0)
-# w.addItem(sp1)
+    def addModel(self):
+        leaf = leadBrick(0,self.zoom)
+        #leaf.rotate(90,1,0,0)
+        site = np.array([230-10,224,681])
+        site -= self.translate
+        site = site/self.zoom
+        leaf.translate(*site)
+        self.widget_GL.addItem(leaf)
+        right = leadBrick(0,self.zoom)
+        site = np.array([230-10,650-230-60,681])
+        site -= self.translate
+        site = site/self.zoom
+        right.translate(*site)
+        self.widget_GL.addItem(right)
+        back = leadBrick(1,self.zoom)
+        back.rotate(-90,0,0,1)
+        site = np.array([180-10,266+100,681])
+        site -= self.translate
+        site = site / self.zoom
+        back.translate(*site)
+        self.widget_GL.addItem(back)
+        self.modelList.extend([leaf,right,back])
 
-#
-pos = np.random.random((10000,3))
-pos *= [10,-10,10]
-pos[0] = (0,0,0)
-color = np.ones((pos.shape[0],4))
-d2 = (pos**2).sum(axis=1)**0.5
-size = np.random.random(size=pos.shape[0])*10
 
-sp2 = gl.GLScatterPlotItem(pos=pos,color=(1,1,1,0.1), size=size)
-w.addItem(sp2)
-# side00 = gl.GLLinePlotItem(pos=np.array([[10,0,0],[10,0,10]]),color=(1,1,1,0.3),width=1)
-# w.addItem(side00)
+
+    def setData(self,data:pd.DataFrame):
+        a0 = pocaAnalizy(data)
+        self.pocaPos = a0.pocaPositions[:,:4]
+        self.plotUpdate()
+
+    @Qt.pyqtSlot(bool)
+    def setModelVisible(self, visible: bool):
+        for i in self.modelList:
+            i.setVisible(visible)
+
+    @Qt.pyqtSlot()
+    def plotUpdate(self):
+        poca = self.pocaPos[(self.pocaPos[:,3] > self.doubleSpinBox_minAngle.value()) & (self.pocaPos[:,3] < self.doubleSpinBox_maxAngle.value())]
+        poca = poca[(poca[:,2] > self.doubleSpinBox_minZ.value()) & (poca[:,2] < self.doubleSpinBox_maxZ.value())]
+        tmp = poca[:,3] / np.max(self.pocaPos[:,3])
+        pos = (poca[:,:3] - self.translate) / self.zoom
+        ColorMap = self.widget_colorMap.colorMap().mapToFloat(data=tmp)
+        self.scatterItem.setData(pos=pos,color = ColorMap,size=tmp*10)
+
+
+
+class leadBrick(gl.GLMeshItem):
+    def __init__(self,modelName = 0,zoom = 30,color = (255,155,33,33)):
+        verts = np.zeros((8, 3))
+        verts[[1, 2, 5, 6], 0] = 200 if modelName == 0 else 100 # x
+        verts[[2, 3, 6, 7], 1] = 50     # y
+        verts[4:, 2] = 100              # z
+        #
+        faces = np.array([
+            [0, 4, 7], [7, 3, 0],   # right
+            [0, 4, 5], [5, 1, 0],   # back
+            [1, 5, 6], [6, 2, 1],   # left
+            [3, 7, 6], [6, 2, 3],   # front
+            [4, 5, 6], [6, 7, 4],   # up
+            [3, 0, 1], [1, 2, 3]    # down
+        ])
+
+        colors = np.empty((faces.shape[0], 4), dtype=float)
+        for i in range(4):
+            colors[:, i] = color[i] / 255
+
+        super(leadBrick, self).__init__(vertexes=verts / zoom,
+                                           faces=faces,
+                                           faceColors=colors,
+                                           smooth=False,
+                                           drawEdges=True,
+                                           edgeColor=(1,1,1,36/255),
+                                           shader='balloon'
+                                          )
+        self.setGLOptions('additive')
+
 
 
 if __name__ == '__main__':
-    import sys
-    QtGui.QApplication.instance().exec_()
+    # 开启GUI进程
+    app = QApplication(sys.argv)
+    try:
+        ex = GL3DWidget()
+        data_L = h5Data("./testData/h5Data/tempData_10.28_10_20_54.h5", "r")
+        data_Z = h5Data("./testData/h5Data/tempData_10.27_14_12_02.h5", "r")
+        data_U = h5Data("./testData/h5Data/tempData_10.30_13_19_39.h5", "r")
+        data = data_U.getData(-2)
+        ex.setData(data)
+        ex.show()
+    except:
+        import traceback
+        traceback.print_exc()
+    app.exec_()
